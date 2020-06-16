6034af8a0a4227c5c7adafce01ee39dcf7c8cb4b
arellaTV <jeremiah.arella@gmail.com>
2020-06-16 02:41:23 -0700

    Adds a symbol dictionary and graphics

---
+++ pages/symbolDictionary.js
@@ -0,0 +1,51 @@
+export default {
+  "A": {
+    offset: "0px",
+  },
+  "B": {
+    offset: "-125px",
+  },
+  "C": {
+    offset: "-250px",
+  },
+  "D": {
+    offset: "-375px",
+
+  },
+  "E": {
+    offset: "-500px",
+  },
+  "F": {
+    offset: "-725px",
+  },
+  "G": {
+    offset: "-850px",
+  },
+  "H": {
+    offset: "-975px",
+  },
+  "I": {
+    offset: "-1100px",
+  },
+  "J": {
+    offset: "-1225px",
+  },
+  "K": {
+    offset: "-1350px",
+  },
+  "L": {
+    offset: "-1475px",
+  },
+  "M": {
+    offset: "-1600px",
+  },
+  "N": {
+    offset: "-1725px",
+  },
+  "O": {
+    offset: "-1850px",
+  },
+  "P": {
+    offset: "-1975px",
+  }
+}
