251d7aad2560b447d554c0b47e00eecf2dfb7ada
arellaTV <jeremiah.arella@gmail.com>
2020-06-16 01:26:26 -0700

    Adds a function to select a random letter

--- pages/api/spin.js
+++ pages/api/spin.js
@@ -1,5 +1,15 @@
-/* Symbol dictionary: Each letter signifies a symbol */
+/* Symbol dictionary:
+   Each letter signifies a symbol
+*/
 const letters = 'ABCDEFGHIJKLMNOP';
+
+const selectRandomLetter = () => {
+  return new Promise((resolve) => {
+    const letterIndex = Math.floor(Math.random() * letters.length);
+    const letter = letters[letterIndex];
+    resolve(letter);
+  });
+}
 
 export default function handler(req, res) {
   res.status(200).json({})
