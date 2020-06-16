b4e7dd018640d795931e220da6a179c22b7d5253
arellaTV <jeremiah.arella@gmail.com>
2020-06-16 01:39:51 -0700

    Returns a 3 character result string

--- pages/api/spin.js
+++ pages/api/spin.js
@@ -11,6 +11,34 @@ const selectRandomLetter = () => {
   });
 }
 
-export default function handler(req, res) {
-  res.status(200).json({})
+export default async function handler(req, res) {
+  let results = "";
+  let firstLetter = "";
+  let secondLetter = "";
+  let thirdLetter = "";
+  let threeInARow = true;
+
+  await selectRandomLetter()
+    .then((letter) => {
+      results += letter;
+      firstLetter = letter;
+    })
+    .then(selectRandomLetter)
+    .then((letter) => {
+      results += letter;
+      secondLetter = letter;
+      if (secondLetter !== firstLetter) {
+        threeInARow = false;
+      }
+    })
+    .then(selectRandomLetter)
+    .then((letter) => {
+      results += letter;
+      thirdLetter = letter;
+      if (thirdLetter !== secondLetter) {
+        threeInARow = false;
+      }
+    });
+
+  res.status(200).json({ results, threeInARow })
 }
